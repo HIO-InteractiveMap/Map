1ef40961582553574b44e5e81e4d730827ac028a
RonanMcdonald <ronanmcdonald@hotmail.co.uk>
2022-10-25 07:42:10 +0100

    SVG Pan - pre-pinch attempt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import { useWindowSize } from '@react-hook/window-size';
+import { UncontrolledReactSVGPanZoom, ReactSVGPanZoom } from 'react-svg-pan-zoom';
+import styled from 'styled-components';
+
+// Style
+import GlobalStyle from './GlobalStyle';
+
+// Assets
+import Map from './assets/Map_Downtown_EN_v2.png';
 
 function App() {
+  const Viewer = useRef(null);
+  const [tool, onChangeTool] = useState('pan');
+  const [value, onChangeValue] = useState(0);
+  const [width, height] = useWindowSize({ initialWidth: 400, initialHeight: 400 });
+
+  useEffect(() => {
+    const view = Viewer.current;
+    view.fitToViewer();
+  }, []);
+
+  const transformOptions = {
+    initialScale: 0.2,
+    minScale: 0.5,
+    maxScale: 2,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <GlobalStyle />
+
+      <Sidebar>
+        <p>website</p>
+      </Sidebar>
+
+      <Content__Container>
+        <ReactSVGPanZoom width={width} height={height} ref={Viewer} value={value} onChangeValue={onChangeValue} tool={tool} onChangeTool={onChangeTool} SVGBackground='black' background='black' options={transformOptions}>
+          <svg width={1885} height={2048}>
+            <g>
+              <image href={Map} />
+            </g>
+          </svg>
+        </ReactSVGPanZoom>
+
+        {/* <UncontrolledReactSVGPanZoom ref={Viewer} width={1885} height={2048} defaultTool='pan'>
+          <svg width={1885} height={2048}>
+            <g>
+              <image href={Map} />
+            </g>
+          </svg>
+        </UncontrolledReactSVGPanZoom> */}
+      </Content__Container>
     </div>
   );
 }
+
+const Content__Container = styled.div`
+  /* padding-top: 20px;
+  padding-left: 20px;
+  padding-bottom: 20px; */
+`;
+
+const Sidebar = styled.div`
+  min-width: 400px;
+  border: 1px solid #cf985f;
+`;
 
 export default App;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 18px;
+    color: white;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.004) !important;
+    background-color: black;
+  }
+
+  .app {
+    display: flex;
+    overflow: hidden;
+    height: 100vh;
+  }
+`;
+export default GlobalStyle;
