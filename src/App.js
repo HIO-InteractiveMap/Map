3a733df7addb3519cd4e3f7b5bf8af205196275e
RonanMcdonald <ronanmcdonald@hotmail.co.uk>
2022-11-02 01:50:43 +0000

    File Structure update. Map res smaller. Filter v1

--- src/App.js
+++ src/App.js
@@ -4,48 +4,56 @@ import styled from 'styled-components';
 // Components
 import Map from './components/Map/Map';
 import Icon from './components/Icon/Icon';
+import Sidebar from './components/Sidebar/Sidebar';
 
 // Style
 import GlobalStyle from './GlobalStyle';
 
 // Data
-import IMAGES from './assets';
-
-const ICON_DATA = {
-  stores: [
-    { name: 'bank', src: IMAGES.bank, size: 50, coordinates: { x: 823, y: 376 } },
-    { name: 'savory', src: IMAGES.savoury, size: 50, coordinates: { x: 565, y: 298 } },
-    { name: 'fertilizer', src: IMAGES.bulletModifer, size: 50, coordinates: { x: 811, y: 268 } },
-    { name: 'pottery', src: IMAGES.vase, size: 50, coordinates: { x: 659, y: 188 } },
-    { name: 'draw', src: IMAGES.drawer, size: 50, coordinates: { x: 655, y: 654 } },
-  ],
-};
+import DATA from './data';
 
 function App() {
-  console.clear();
+  console.log('');
+  const [ICON_DATA, setICON_DATA] = useState(DATA);
+
+  const handleToggle = (id, layer) => {
+    console.log(id);
+
+    setICON_DATA((current) =>
+      current.map((obj) => {
+        if (obj.id == id) {
+          return { ...obj, visable: !obj.visable };
+        }
+
+        return obj;
+      })
+    );
+  };
+
+  console.log(ICON_DATA);
 
   const icons = [];
-
-  Object.entries(ICON_DATA).map(([key, value]) => {
-    value.map((element, index) => {
-      console.log(element);
+  ICON_DATA.map((icon) => {
+    icon.visable &&
       icons.push(
         <Icon
-          key={index}
-          x={element.coordinates.x}
-          y={element.coordinates.y}
-          src={element.src}></Icon>
+          key={icon.id}
+          x={icon.coordinates.x}
+          y={icon.coordinates.y}
+          src={icon.src}
+          size={icon.size}
+        />
       );
-    });
   });
 
   return (
     <div className='app'>
       <GlobalStyle />
 
-      {/* <Sidebar>
-        <p>Sidebar</p>
-      </Sidebar> */}
+      <Sidebar
+        ICON_DATA={ICON_DATA}
+        handleToggle={handleToggle}
+      />
 
       <Content__Container>
         <Map>{icons}</Map>
@@ -69,11 +77,6 @@ const Content__Container = styled.div`
     cursor: -moz-grabbing;
     cursor: -webkit-grabbing;
   }
-`;
-
-const Sidebar = styled.div`
-  min-width: 400px;
-  border: 1px solid #cf985f;
 `;
 
 export default App;
